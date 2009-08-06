3a6cb5c2a6dd0555357744624c31df7a6e79d6a6
Mike Korobov <kmike84@gmail.com>
2009-08-07 00:58:08 +0600

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
       name='django-generic-images',
-      version='0.13',
+      version='0.14',
       author='Mikhail Korobov',
       author_email='kmike84@gmail.com',
       url='http://bitbucket.org/kmike/django-generic-images/',      
