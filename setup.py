d1e199eacee62a8f61bd45c900e4876b5127e319
Mike Korobov <kmike84@gmail.com>
2009-08-13 17:00:24 +0600

    Rrr. Fix manifest.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
       name='django-generic-images',
-      version='0.21',
+      version='0.22',
       author='Mikhail Korobov',
       author_email='kmike84@gmail.com',
       url='http://bitbucket.org/kmike/django-generic-images/',      
