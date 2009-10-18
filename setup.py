d117606ec042c6970220291681c67d7280b87fbf
Mike Korobov <kmike84@gmail.com>
2009-10-18 07:04:00 +0600

    Remove .pyc files from distribution.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
       name='django-generic-images',
-      version='0.35',
+      version='0.35.1',
       author='Mikhail Korobov',
       author_email='kmike84@gmail.com',
       url='http://bitbucket.org/kmike/django-generic-images/',
