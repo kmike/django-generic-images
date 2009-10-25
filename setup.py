236ff14527e7cc9b49e7018dff87d164f5309e6e
Mike Korobov <kmike84@gmail.com>
2009-10-26 02:27:39 +0500

    Bump version. Rebuild documentation.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
       name='django-generic-images',
-      version='0.35.1',
+      version='0.35.2',
       author='Mikhail Korobov',
       author_email='kmike84@gmail.com',
       url='http://bitbucket.org/kmike/django-generic-images/',
