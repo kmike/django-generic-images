b9d9945d8ebe319cbc49546c6b44ff63f4c93361
Mike Korobov <kmike84@gmail.com>
2009-11-01 21:08:55 +0500

    Do not show uploader in add form. Fix #4.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
       name='django-generic-images',
-      version='0.35.6',
+      version='0.35.7',
       author='Mikhail Korobov',
       author_email='kmike84@gmail.com',
       url='http://bitbucket.org/kmike/django-generic-images/',
